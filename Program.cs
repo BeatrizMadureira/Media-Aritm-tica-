10d64e492db513b098fbb8a279197c1a7301c851
BeatrizMadureira <beatrizmareiralimamadureira@gmail.com>
2025-10-03 20:12:19 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("MediaAritmedica");
+double nota1, nota2, nota3, media;
+
+Console.WriteLine("Média Aritmética de 3 números\n");
+
+Console.Write("Escreva o primeiro número: ");
+nota1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Escreva o segundo número: ");
+nota2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Escreva o terceiro número: ");
+nota3 = Convert.ToDouble(Console.ReadLine());
+
+media = (nota1 + nota2 + nota3) / 3;
+
+Console.WriteLine($"\nMédia: {media:N1}");
